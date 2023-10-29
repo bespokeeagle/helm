5c928b3b581c8060f0cb354f13e1e6af5a8f5d72
bespokeeagle <symbolchimere@gmail.com>
2023-10-29 03:53:53 +0200

    debug

--- changescript.sh
+++ changescript.sh
@@ -9,7 +9,7 @@ do
     then
         echo "i am a $directory directory"
         echo "1 $GITHUB_REF_NAME 2 $GITHUB_HEAD_REF 3 $GITHUB_BASE_REF	"
-        TOBEPACKAGED=(`git diff --name-only origin/$GITHUB_REF_NAME..origin/$GITHUB_BASE_REF| grep $directory/Chart.yaml |  cut -f 1 -d '/'`)
+        TOBEPACKAGED=(`git diff --name-only origin/$$GITHUB_HEAD_REF..origin/$GITHUB_BASE_REF| grep $directory/Chart.yaml |  cut -f 1 -d '/'`)
         echo "Packaging $TOBEPACKAGED"
         helm package $TOBEPACKAGED
         rm -r demo
