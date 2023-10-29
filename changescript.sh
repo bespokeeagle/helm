a2ce77ad8132b1a0133219834370f03a85413dcd
bespokeeagle <symbolchimere@gmail.com>
2023-10-29 03:57:25 +0200

    fetching

--- changescript.sh
+++ changescript.sh
@@ -9,6 +9,7 @@ do
     then
         echo "i am a $directory directory"
         echo "1 $GITHUB_REF_NAME 2 $GITHUB_HEAD_REF 3 $GITHUB_BASE_REF	"
+        git fetch
         TOBEPACKAGED=(`git diff --name-only origin/$GITHUB_HEAD_REF..origin/$GITHUB_BASE_REF| grep $directory/Chart.yaml |  cut -f 1 -d '/'`)
         echo "Packaging $TOBEPACKAGED"
         helm package $TOBEPACKAGED
